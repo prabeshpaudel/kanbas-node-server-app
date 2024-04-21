67426b033265804cd0d62d9989a454fc4cfd2eed
Prabesh Paudel <ppaudel@Prabeshs-MacBook-Pro.local>
2024-04-21 10:29:37 -0400

    tweak functionality

--- Kanbas/questions/schema.js
+++ Kanbas/questions/schema.js
@@ -24,7 +24,7 @@ const questionSchema = new mongoose.Schema(
       {
         text: {
           type: String,
-          required: true,
+          // required: true,
         },
         isCorrect: {
           type: Boolean,
